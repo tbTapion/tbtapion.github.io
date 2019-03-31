5c721b764200d7583b7f9f9b7e3f0c8e77bd3146
Andreas L <tbTapion@users.noreply.github.com>
2019-03-31 07:07:13 +0200

    linkstuff

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,26 @@ function init(){
       };
       firebase.initializeApp(config);
     
-    var hash = document.location.hash;
-    document.getElementById("amqstats").hidden = (hash == "amqstats");
-    document.getElementById("home").hidden = (hash == "amqstats");
+    var hash = window.location.hash.substring(1);
+    console.log(hash);
+    if(hash =="amqstats"){
+        document.getElementById("amqstats").hidden = false;
+        document.getElementById("home").hidden = true;
+    }else{
+        document.getElementById("amqstats").hidden = true;
+    }
+}
+
+function changepage(event){
+    var element = event.target || event.srcElement;
+    console.log(element);
+    var type = element.id
+    console.log(type);
+    if(type == "link-home"){
+        document.getElementById("amqstats").hidden = true;
+        document.getElementById("home").hidden = false;
+    }else if(type == "link-stats"){
+        document.getElementById("amqstats").hidden = false;
+        document.getElementById("home").hidden = true;
+    }
 }
