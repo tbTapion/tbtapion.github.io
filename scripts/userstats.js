2d7f53d64c33764a0dc49a12e5124c3160107dbc
Andreas L <tbTapion@users.noreply.github.com>
2019-03-31 18:53:02 +0200

    call from database to see single user stats works now

--- scripts/userstats.js
+++ scripts/userstats.js
@@ -1,3 +1,12 @@
 function renderUser(doc){
-    
+    var values = doc.data();
+    var username = values.username;
+    var correctguesses =values.correctguesses;
+    var frommal = values.frommal;
+    var numberofgames = values.numberofgames;
+
+    document.getElementById("username").innerHTML = username;
+    document.getElementById("correctguesses").innerHTML = "Correct Guesses: " + correctguesses;
+    document.getElementById("frommal").innerHTML = "From Anime List: " + frommal;
+    document.getElementById("numberofgames").innerHTML = "Number of Games:" + numberofgames;
 }
