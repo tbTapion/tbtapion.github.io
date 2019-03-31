f6df2fc0286cb93e57b39766cbbc4432706d0006
Andreas L <tbTapion@users.noreply.github.com>
2019-03-31 17:21:01 +0200

    added more firebase functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,22 @@
+
+
+function renderUser(doc){
+    var div = document.createElement("div");
+    var name = document.createElement("div");
+    
+    name.innerHTML = doc.data().username;
+    name.id = 'result-text';
+    div.id = "result-box";
+    div.appendChild(name);
+
+    document.getElementById('searchResults').appendChild(div);
+}
+
 function updateSearch(value){
-    console.log(value);
+    database.collection('users').where("username", 'array-contains', value.toString()).get().then((snapshot, value) => {
+        snapshot.docs.forEach(doc => {
+            console.log(doc);
+            renderUser(doc);
+        });
+    });
 }
