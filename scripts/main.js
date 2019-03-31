f7be5c45fcac42866790dc7839d1e27e163b3576
Andreas L <tbTapion@users.noreply.github.com>
2019-03-31 14:11:20 +0200

    updatesss

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,5 @@
-function setupdependencies(){
-    var fileref=document.createElement('script');
-    fileref.setAttribute("type","text/javascript");
-    fileref.setAttribute("src", "https://www.gstatic.com/firebasejs/5.9.2/firebase.js");
-    var fileref2=document.createElement('script');
-    fileref2.setAttribute("type","text/javascript");
-    fileref2.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
-}
-
 function init(){
-    setupdependencies();
-    var config = {
-        apiKey: "AIzaSyD8CBsKmAom07TW_0BCYvoR3Wa0Z1sHXFo",
-        authDomain: "amqstats.firebaseapp.com",
-        databaseURL: "https://amqstats.firebaseio.com",
-        projectId: "amqstats",
-        storageBucket: "amqstats.appspot.com",
-        messagingSenderId: "327250267342"
-      };
-      firebase.initializeApp(config);
-    
+    loadFirebase();
     var hash = window.location.hash.substring(1);
     console.log(hash);
     if(hash =="amqstats"){
