faad6a778b2da8a1aa9383ec8120fd4da30df5bf
Andreas L <tbTapion@users.noreply.github.com>
2019-04-03 02:11:03 +0200

    update to player-boxes

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,19 @@
 function renderUser(doc){
     var div = document.createElement("div");
     var name = document.createElement("div");
+    var games = document.createElement("div");
     
     name.innerHTML = doc.data().username;
+    name.innerHTML = doc.data().numberofgames;
     name.id = 'result-text';
+    games.id = 'result-text'
     div.id = "result-box";
     div.onclick = function () {
         window.location.href = "/stats/userstats.html#" + name.innerHTML;
         console.log(window.location.href);
     };
     div.appendChild(name);
+    div.appendChild(games);
 
     document.getElementById('searchResults').appendChild(div);
 }
