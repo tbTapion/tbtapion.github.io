bb3500736ca8482f33f25563e18309e3c424cb82
Andreas L <tbTapion@users.noreply.github.com>
2019-04-03 02:13:02 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ function renderUser(doc){
     name.innerHTML = doc.data().username;
     name.innerHTML = doc.data().numberofgames;
     name.id = 'result-text';
-    games.id = 'result-text'
+    games.id = 'result-text';
     div.id = "result-box";
     div.onclick = function () {
         window.location.href = "/stats/userstats.html#" + name.innerHTML;
