df3318130a394fe03c63cb59e774bdda08edda5f
Andreas L <tbTapion@users.noreply.github.com>
2025-05-17 01:15:28 +0200

    routing fix

--- mkth/js/router.js
+++ mkth/js/router.js
@@ -16,8 +16,14 @@ const url = new URL(window.location.href);
 
 Object.keys(routes).forEach((key) => {
     if(url.pathname.startsWith(key)) {
-        if(routes[key].restricted) {
+        if(routes[key].restricted && !appState.tournament) {
             window.location.replace(routes[key].redirect)
         }
     }
 })
+
+const router = {
+    replace(target) {
+        window.location.replace("/mkth"+target);
+    }
+}
